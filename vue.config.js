2c1accfff69dea128de882ba2fea69d291051818
yuezengchen <yuezengchen_job@163.com>
2020-04-07 15:17:46 +0800

    refactor-修改vue.config.js里webpack配置 打包的js文件分开打包

--- vue.config.js
+++ vue.config.js
@@ -28,7 +28,7 @@ module.exports = {
   outputDir: 'dist',
 
   // default:'dist' 放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录。
-  assetsDir: '',
+  assetsDir: 'static',
 
   // default:'index.html' 指定生成的 index.html 的输出路径 (相对于 outputDir)。也可以是一个绝对路径。
   indexPath: 'index.html',
@@ -52,7 +52,45 @@ module.exports = {
       }
     }
   },
-
+  chainWebpack(config) {
+    config.when(process.env.NODE_ENV !== 'development',
+      config => {
+        config
+          .plugin('ScriptExtHtmlWebpackPlugin')
+          .after('html')
+          .use('script-ext-html-webpack-plugin', [{
+            // `runtime` must same as runtimeChunk name. default is `runtime`
+            inline: /runtime\..*\.js$/
+          }])
+          .end();
+        config
+          .optimization.splitChunks({
+            chunks: 'all',
+            cacheGroups: {
+              libs: {
+                name: 'chunk-libs',
+                test: /[\\/]node_modules[\\/]/,
+                priority: 10,
+                chunks: 'initial' // only package third parties that are initially dependent
+              },
+              elementUI: {
+                name: 'chunk-elementUI', // split elementUI into a single package
+                priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+                test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
+              },
+              commons: {
+                name: 'chunk-commons',
+                test: resolve('src/components'), // can customize your rules
+                minChunks: 3, //  minimum common number
+                priority: 5,
+                reuseExistingChunk: true
+              }
+            }
+          });
+        config.optimization.runtimeChunk('single');
+      }
+    );
+  },
   css: {
     // type: boolean || Object default: 生产环境下true,开发环境下false
     extract: process.env.NODE_ENV === 'production',
