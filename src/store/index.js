f6053e2090c2a8b86832f3bb4d6bdd64774dcf77
yuezengchen <yuezengchen_job@163.com>
2020-05-21 13:32:04 +0800

    refactor 更新readme 引入element样式

--- src/store/index.js
+++ src/store/index.js
@@ -8,14 +8,11 @@ Vue.use(Vuex);
 const modulesFiles = require.context('./modules', true, /\.js$/);
 
 const modules = modulesFiles.keys().reduce((modules, modulePath) => {
-  // set './app.js' => 'app'
   const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1');
   const value = modulesFiles(modulePath);
   modules[moduleName] = value.default;
   return modules;
 }, {});
-console.log(modules);
-console.log('****');
 const store = new Vuex.Store({
   modules,
   getters
