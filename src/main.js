369507f5189bb45db5fb1db8ed8d08234ba94810
yuezengchen <yuezengchen_job@163.com>
2020-05-29 11:14:51 +0800

    feature-引入mock.js iconfont permission 设置cookie过期时间

--- src/main.js
+++ src/main.js
@@ -1,20 +1,15 @@
-import Vue from 'vue';
-
-import Cookies from 'js-cookie';
-
-import 'normalize.css/normalize.css'; // 一种CSS reset的替代方案
-
+import '@/styles/index.scss'; // global css
 import Element from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-
-import '@/styles/index.scss'; // global css
-
+import Cookies from 'js-cookie';
+import 'normalize.css/normalize.css'; // 一种CSS reset的替代方案
+import Vue from 'vue';
+// import '@/assets/iconfont/iconfont.css';
 import App from './App.vue';
+import * as filters from './filters'; // global filters
 import router from './router';
 import store from './store';
-
-import * as filters from './filters'; // global filters
-
+if (process.env.NODE_ENV !== 'production') require('./mock');
 Vue.use(Element, {
   size: Cookies.get('size') || 'medium' // set element-ui default size
   // i18n: (key, value) => i18n.t(key, value)
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <navbar />
+    dashboard
   </div>
 </template>
 <script>
-import Navbar from '@/components/Navbar';
+
 export default {
   name: '',
-  components: { Navbar },
+  components: { },
   data() {
     return {
     };
